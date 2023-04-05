08c962f39f468e5c5273e3d439673846f301e529
Stas.S <stas.eco82@gmail.com>
2023-04-05 14:01:24 +0300

    pagination  video 99

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,6 +32,17 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination (// page=2&limit=10)
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('this page does not exist');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
