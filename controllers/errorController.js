8fd93a641c5452b6fed425e6e795a8ee2b904780
Stas.S <stas.eco82@gmail.com>
2023-04-12 15:33:37 +0300

    Handling Duplicate DB Fields video 120

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,12 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (errmsg, err) => {
+  const value = errmsg.match(/([""'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value:${value}. Please use another value !`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -43,6 +49,7 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err.errmsg, error);
 
     sendErrorProd(error, res);
   }
