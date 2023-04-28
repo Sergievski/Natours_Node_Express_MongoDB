858993645d3b368411e9c995f6694ff76339eb40
Stas.S <stas.eco82@gmail.com>
2023-04-28 15:54:08 +0300

    Updaiting the current user password - Video 138

--- models/userModel.js
+++ models/userModel.js
@@ -57,7 +57,10 @@ userSchema.pre('save', async function (next) {
 });
 
 userSchema.pre('save', function (next) {
-  if (!this.isModified('password')) return next();
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
 });
 
 userSchema.methods.correctPassword = async function (
