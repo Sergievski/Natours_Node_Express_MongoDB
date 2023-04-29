8a1d8e29deb51d865eef07620e767d0d3196f49c
Stas.S <stas.eco82@gmail.com>
2023-04-29 15:34:25 +0300

    Modelling Tour Guides: Child Referencing - Video 152

--- models/tourModel.js
+++ models/tourModel.js
@@ -100,7 +100,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -118,10 +123,10 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-});
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+// });
 
 // QUERY MIDDLEWARE :
 tourSchema.pre(/^find/, function (next) {
