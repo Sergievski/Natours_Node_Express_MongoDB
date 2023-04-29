8d312a7c7af4164a0427fb0c92d77e022dd99f3c
Stas.S <stas.eco82@gmail.com>
2023-04-29 15:19:15 +0300

    Modelling Tour Guides: Embedding - Video 151

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 //udeconst validator = require('validator'); // there is an example in line 13
 
 const tourSchema = new mongoose.Schema(
@@ -99,6 +100,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -114,6 +116,11 @@ tourSchema.virtual('durationWeeks').get(function () {
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
 });
 
 // QUERY MIDDLEWARE :
