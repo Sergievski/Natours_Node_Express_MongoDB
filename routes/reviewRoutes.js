df8d4feb55afd115e9ed4843b7424e4ea03e4bf5
Stas.S <stas.eco82@gmail.com>
2023-05-05 10:09:51 +0300

    Creating and Getting Reviews - Video 155

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const tourController = require('../controllers/reviewController');
+const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.route('/').get(tourController.getAllReviews);
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
 
 module.exports = router;
