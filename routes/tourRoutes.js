05321029bff6961ede051123c6c986ee14fada97
Stas.S <stas.eco82@gmail.com>
2023-05-06 13:21:18 +0300

    Nested Routes with Express - Video 159

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);  ->
+
+// POST  /tour/234dfsd/reviews
+// GET  /tour/234dfsd/reviews
+// GET  /tour/234dfsd/reviews/94fdksdfh73
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -27,18 +41,6 @@ router
     authController.protect,
     authController.restrictTo('admin'),
     tourController.deleteTour
-  );
-
-// POST  /tour/234dfsd/reviews
-// GET  /tour/234dfsd/reviews
-// GET  /tour/234dfsd/reviews/94fdksdfh73
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
