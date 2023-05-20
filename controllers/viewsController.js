e8e1b6cd452bc03fb2ab0219b185101433941eee
Stas.S <stas.eco82@gmail.com>
2023-05-20 19:30:40 +0300

    Building the Tour Overview - Part 1 - Video 182

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,22 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tout data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+
+  // 3) Render that template using tour data from 1
+
   res.status(200).render('overview', {
     title: 'All tours',
+    tours: tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
