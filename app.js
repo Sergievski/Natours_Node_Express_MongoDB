cc0d27969a530d13ee31c12b6cd907ceebadfb7c
Stas.S <stas.eco82@gmail.com>
2023-07-07 20:35:53 +0300

    Finishing Payments with Stripe Webhooks - Middle of Video 227

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
+
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
@@ -51,6 +53,12 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP , please try again in a hour',
 });
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
